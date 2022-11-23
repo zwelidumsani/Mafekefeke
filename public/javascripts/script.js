aac92f7351d15e966cc438f6bc11afdc402c5ba9
zwelidumsani <zwelidumsani@gmail.com>
2022-11-23 14:11:15 +0200

    Latest update

---
+++ public/javascripts/script.js
@@ -0,0 +1,7 @@
+var menuIcon = document.querySelector(".icon-menu");
+var sidebar = document.querySelector(".sidebar");
+
+menuIcon.onclick = function(){
+	 sidebar.classList.toggle("small-sidebar");
+}
+
