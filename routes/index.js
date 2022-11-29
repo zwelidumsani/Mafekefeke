58279531b63900b53e262d808bcf8e0a82ef688c
zwelidumsani <zwelidumsani@gmail.com>
2022-11-29 14:19:18 +0200

    Latest changes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mafekefeke' });
 });
 
+router.get('/play', function(req, res, next){
+	 res.render('playvideo');
+});
+ 
 module.exports = router;
